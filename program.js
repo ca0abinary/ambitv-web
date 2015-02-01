5769ead66a4dc4b29d271fc905f11f1a1b637383
ca0abinary <jondemarks@gmail.com>
2015-02-01 10:42:16 -0500

    Initial commit

---
+++ program.js
@@ -0,0 +1,28 @@
+var http = require('http')
+var exec = require('child_process').exec
+
+var connect = require('connect')
+var app = connect()
+
+var compression = require('compression')
+app.use(compression())
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded())
+
+var serveStatic = require('serve-static')
+app.use(serveStatic('www'))
+
+app.use('/api', function apiMiddleware(req, res, next){
+  if (/edgeLit/i.test(req.url)) { // /api/edgeLit
+    exec('screen -d -m /usr/sbin/ambi-tv &')
+    res.end(JSON.stringify({operation:'edgeLit',status:'complete'}))
+  }
+  else if (/lightsOff/i.test(req.url)) { // /api/lightsOff
+    exec('pkill -SIGINT ambi')
+    res.end(JSON.stringify({operation:'lightsOff',status:'complete'}))
+  }
+  next()
+})
+
+http.createServer(app).listen(8080)
